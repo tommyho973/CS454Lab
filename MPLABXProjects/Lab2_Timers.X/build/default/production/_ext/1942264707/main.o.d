defc0df50dcc4dcec994a6e625543bcfae1bbda6
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2024-01-31 09:44:00 -0500

    Add files via upload

---
+++ MPLABXProjects/Lab2_Timers.X/build/default/production/_ext/1942264707/main.o.d
@@ -0,0 +1,10 @@
+build/default/production/_ext/1942264707/main.o: ../Lab1.X/main.c \
+ ../Lab1.X/lcd.h ../Lab1.X/types.h ../Lab1.X/led.h ../Lab1.X/joystick.h
+
+../Lab1.X/lcd.h:
+
+../Lab1.X/types.h:
+
+../Lab1.X/led.h:
+
+../Lab1.X/joystick.h:
