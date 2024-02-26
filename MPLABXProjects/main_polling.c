cfeee9514492a35230be88e53c6436c5b1b2aaf6
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2024-02-26 15:17:47 -0500

    Add files via upload

---
+++ MPLABXProjects/main_polling.c
@@ -0,0 +1,313 @@
+//do not change the order of the following 3 definitions
+#define FCY 12800000UL 
+#include <stdio.h>
+#include <libpic30.h>
+
+#include <p33Fxxxx.h>
+#include "types.h"
+#include "uart.h"
+#include "crc16.h"
+#include "lab3.h"
+#include "lcd.h"
+#include "timer.h"
+
+// Primary (XT, HS, EC) Oscillator without PLL
+_FOSCSEL(FNOSC_PRIPLL);
+// OSC2 Pin Function: OSC2 is Clock Output - Primary Oscillator Mode: XT Crystanl
+_FOSC(OSCIOFNC_ON & POSCMD_XT);
+// Watchdog Timer Enabled/disabled by user software
+_FWDT(FWDTEN_OFF);
+// Disable Code Protection
+_FGS(GCP_OFF);   
+
+int timer_expired = 0;
+
+uint8_t bufferArray[MSG_MAX_LEN];
+int writer = 0;
+int reader = 0; 
+int start_byte_index= 0;
+
+uint8_t start_byte=0;
+uint8_t data_array[MSG_BYTES_MSG];
+
+uint16_t crc = 0;
+uint8_t crc1 = 0;
+uint8_t crc2 = 0;
+uint16_t calculated_crc = 0;
+
+uint8_t expected_length = 0;
+uint8_t actual_length = 0;
+
+int failed_sends = 0;
+uint8_t ACK= 0x1;
+uint8_t NACK = 0x0;
+    
+    char message[MSG_BYTES_MSG];
+
+
+void __attribute__((__interrupt__)) _T1Interrupt(void){
+    
+    CLEARBIT(IFS0bits.T1IF);
+    timer_expired = 1;
+}
+
+//Not supposed to use Interrupt, we're supposed to use polling, which is just constantly checking the receive until it returns 0
+// void __attribute__((__interrupt__, no_auto_psv)) _U2RXInterrupt() {
+//     bufferArray[writer]= usart_getc();
+//     writer = (writer + 1) % MSG_MAX_LEN;
+//     IFS1bits.U2RXIF = 0;
+// }
+
+//Implemented usart_getc() in the uart.c to do polling instead of interrupt
+
+// uint8_t usart_getc(){
+//     // Fresh data
+//     uint8_t data = 0;
+    
+//     //Set the timer
+//     set_timer1(32767);
+        
+//     while(1){
+//         if (uart2_recv(&data) == 0){
+//             if (data == 0x0) {
+//                 start_byte_index = reader;
+//             }
+//             reader = (reader + 1) % MSG_MAX_LEN;
+//             break;
+//         }
+        
+//         if (timer_expired==1){
+//             CLEARBIT(IEC0bits.T1IE);
+//             return -1; // NEEDS TO BE FIXED ITS NOT -1 ITS SMTH ELSE
+//         }
+//     }
+    
+//     CLEARBIT(IEC0bits.T1IE);
+    
+//     return data;
+    
+//        while(1) {
+//
+//            if((uart2_recv(&start_byte)==0) && ( start_byte== 0x0)){
+//                break;
+//            }
+//            if(timer_expired == 1){
+//                CLEARBIT(IEC0bits.T1IE);
+//                printf("MESSED UP with first byte time");
+//                // Indicate time error            break;
+//                uart2_send_8(NACK);
+//            }
+//        }
+//
+//        // Reading the received CRC
+//        if((uart2_recv(&crc_bytes[0]) != 0) || (uart2_recv(&crc_bytes[1]) != 0)){
+//            CLEARBIT(IEC0bits.T1IE);
+//            printf("MESSED UP with crc");
+//            uart2_send_8(NACK);
+//    //        return NACK;
+//        }
+//        crc = ((uint16_t)crc_bytes[0]) | ((uint16_t)(crc_bytes[1]) << 8);
+//
+//        // Reading message body length
+//        if (uart2_recv(&expected_length) != 0){
+//            CLEARBIT(IEC0bits.T1IE);
+//            printf("MESSED UP with length");
+//            uart2_send_8(NACK);
+//    //        return NACK;
+//        }
+//        
+//
+//        // Read the message itself
+//        while(actual_length < expected_length){
+//            if(uart2_recv(&data) == 0){
+//                data_array[data_counter] = data;
+//                data_counter++;
+//                calculated_crc = crc_update(calculated_crc, data);
+//            }
+//
+//            if(timer_expired == 1){
+//                printf("MESSED UP with body message time");
+//                uart2_send_8(NACK);
+//    //            return NACK;
+//            }
+//        }
+    
+//}
+
+int main(void)
+{	
+	/* Q: What is my purpose? */
+	/* A: You pass butter. */
+	/* Q: Oh. My. God. */\
+    
+    uint16_t message_length = MSG_MAX_LEN;
+    // bufferArray = (uint8_t*)malloc(MSG_MAX_LEN * sizeof(uint8_t));
+    data_array = (uint8_t*)malloc(MSG_BYTES_MSG * sizeof(uint8_t));
+    
+    uart2_init(BAUD_9600);
+    while(1){
+    int i = 0;
+    data_array[i] = usart_getc();
+    set_timer1(32767);
+    while(timer_expired != 1){
+        i++;
+        data_array[i] = usart_getc();
+        if(i == 3){
+            message_length = data_array[3] + 4;
+        }
+        if(i >= message_length){
+            //TURN OFF TIMER HERE
+            break;
+        }
+
+    };
+    
+    crc1 = data_array[1];
+    crc2 = data_array[2];
+    int j = 4;
+    crc = ((uint16_t)crc1 | ((uint16_t)crc2 << 8));
+    for(j = 4; j < message_length; j++){
+        calculated_crc = crc_update(calculated_crc,data_array[j]);
+    }
+    // Disable receiver ?????? -> ASK QUESTION PLS
+    
+    if(crc == calculated_crc){
+        // Data chilling
+            printf("YOU CHILLING");
+            // print the failed on the lcd
+            lcd_locate(0,0);
+            lcd_clear_row(0);
+            lcd_printf("Recv fail: %d", failed_sends);
+            gotoLine(1);
+            lcd_clear_row(1);
+            lcd_printf("recv_buf:");
+            lcd_printf("%d %x %x %d", data_array[0], data_array[1], data_array[2], data_array[3]);
+            gotoLine(2);
+            lcd_clear_row(2);
+            int k;
+            for(k = 4; k < message_length; k++){
+                lcd_printf("%d ", data_array[k]);
+                char c = data_array[k];
+                message[k-4] = c;
+            }
+            gotoLine(3);
+            lcd_printf("crc: %x crc: %x",calculated_crc, crc);
+            gotoLine(4);
+            lcd_printf("str: %s", message);
+            uart2_send_8(ACK);
+    }
+    else{
+            printf("YOU NOOOTTTTTTT CHILLING");
+            failed_sends++;
+            uart2_send_8(NACK);
+    }
+    }
+    // start_byte = bufferArray[start_byte_index];
+    // start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
+    
+    // crc1 = (bufferArray[start_byte_index]);
+    // start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
+    // crc2 = ((bufferArray[start_byte_index]));
+    
+    // crc = ((uint16_t)crc1 | ((uint16_t)crc2 << 8));
+    
+    // start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
+    // expected_length = bufferArray[start_byte_index];
+    
+    
+    // while(start_byte_index != reader) {
+    //     start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
+    //     data_array[actual_length] = bufferArray[start_byte_index];
+    //     actual_length++;
+    //     calculated_crc = crc_update(calculated_crc, bufferArray[start_byte_index]);
+        
+    // }
+    
+    // if((crc == calculated_crc) && (expected_length == actual_length)){
+    //         // Data chilling
+    //         printf("YOU CHILLING");
+    //         // print the failed on the lcd
+    //         lcd_locate(0,0);
+    //         lcd_clear_row(0);
+    //         lcd_printf("Recv fail: %d", failed_sends);
+    //         gotoLine(1);
+    //         lcd_clear_row(1);
+    //         lcd_printf("recv_buf:");
+    //         lcd_printf("%d %x %x %d", start_byte, crc1, crc2, expected_length);
+    //         gotoLine(2);
+    //         lcd_clear_row(2);
+    //         int i = 0;
+    //         while(i < actual_length){
+    //             lcd_printf("%d ", data_array[i]);
+    //             char c = data_array[i];
+    //             message[i] = c;
+    //             i++;
+    //         }
+    //         gotoLine(3);
+    //         lcd_printf("crc: %x crc: %x",calculated_crc, crc);
+    //         gotoLine(4);
+    //         lcd_printf("str: %s", message);
+    //         uart2_send_8(ACK);
+    //     }
+    
+    // else{
+    //         printf("YOU NOOOTTTTTTT CHILLING");
+    //         failed_sends++;
+    //         uart2_send_8(NACK);
+    // }
+    
+    
+    // Here is where you calculate the data array and also
+    
+    // Check data integrity (see below)
+    
+    
+//    while(1) {
+//    //data first seen, fire interrupt 1 second after
+//        // Stop timer we done
+//        CLEARBIT(IEC0bits.T1IE);
+//
+//        if((crc == calculated_crc) && (expected_length == actual_length)){
+//            // Data chilling
+//            printf("YOU CHILLING");
+//            // print the failed on the lcd
+//            lcd_locate(0,0);
+//            lcd_clear_row(0);
+//            lcd_printf("Recv fail: %d", failed_sends);
+//            gotoLine(1);
+//            lcd_clear_row(1);
+//            lcd_printf("recv_buf:");
+//            lcd_printf("%d %x %x %d", start_byte, crc_bytes[0], crc_bytes[1], expected_length);
+//            gotoLine(2);
+//            lcd_clear_row(2);
+//            int i = 0;
+//            while(i < actual_length){
+//                lcd_printf("%d ", data_array[i]);
+//                char c = data_array[i];
+//                message[i] = c;
+//                i++;
+//            }
+//            gotoLine(3);
+//            lcd_printf("crc: %x crc: %x",calculated_crc, crc);
+//            gotoLine(4);
+//            lcd_printf("str: %s", message);
+//            uart2_send_8(ACK);
+//        }
+//        else{
+//            printf("YOU NOOOTTTTTTT CHILLING");
+//            failed_sends++;
+//            uart2_send_8(NACK);
+//        }
+//    }
+    
+    // Questions:
+    // 1. How do we handle the NACK such that we have to resend the message with alot of NACKS or handle it differently and do we need this big while loop
+    // 2. Edge case: how do we handle overflow buffer and what is the wrap around method.
+    // 3. How do we run this together to test lol
+
+    
+    
+    return 0;    
+    
+}	
+
---
+++ MPLABXProjects/uart_polling.c
@@ -0,0 +1,72 @@
+/****************************************************/
+/*                                                  */
+/*   CS-454/654 Embedded Systems Development        */
+/*   Instructor: Renato Mancuso <rmancuso@bu.edu>   */
+/*   Boston University                              */
+/*                                                  */
+/*   Description: lab3 UART2 init,TX,RX functions   */
+/*                                                  */
+/****************************************************/
+
+#include <p33FJ256MC710.h>
+
+#include "uart.h"
+
+dataArray = (uint8_t*)malloc(MSG_MAX_LEN * sizeof(uint8_t));
+int read = -1;
+int write = 0;
+
+inline void uart2_init(uint16_t baud)
+{
+    U2MODEbits.UARTEN = 0;
+    IEC1bits.U2RXIE = 0;
+    IEC1bits.U2TXIE = 0;
+    IFS1bits.U2RXIF =0;
+    IFS1bits.U2TXIF = 0;
+    TRISFbits.TRISF2 = 1; // Check if these are for UART2 as well
+    TRISFbits.TRISF3 = 0;
+    U2MODEbits.BRGH = 0;
+    U2BRG = (uint32_t)800000 / baud -1;
+    U2MODE = 0;
+    U2MODEbits.RTSMD = 0;
+    U2MODEbits.UEN = 0;
+    U2MODE |= 0x00;
+    U2MODEbits.UARTEN = 1;
+    U2STA = 0;
+    U2STAbits.UTXEN = 1;
+}
+
+void uart2_send_8(int8_t data)
+{
+	/* Implement me please. */
+    while(U2STAbits.UTXBF);
+    U2TXREG = data;
+    while(!U2STAbits.TRMT);
+}
+
+int8_t uart2_recv(uint8_t* data)
+{
+	/* Implement me please. */
+    if(U2STAbits.OERR){
+        U2STAbits.OERR=0;
+    }
+    if(U2STAbits.URXDA){
+        if(write != read){
+            *data = U2RXREG & 0x00FF;
+            write = (write + 1) % MSG_MAX_LEN;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+uint8_t usart_getc(){
+    while(uart2_recv(data_array[write])!= 0);
+    uint8_t return_num = -1;
+    if((read + 1)% MSG_MAX_LEN != write){
+        return_num = data_array[read];
+        read = (read + 1) % MSG_MAX_LEN;
+    }
+    return return_num;
+
+}
