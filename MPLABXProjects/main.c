9aac7662b66bd6eeaf346a9d84afb15bdd1a072c
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2024-02-27 18:27:56 -0500

    Add files via upload

--- MPLABXProjects/main.c
+++ MPLABXProjects/main.c
@@ -23,9 +23,6 @@ _FGS(GCP_OFF);
 int timer_expired = 0;
 
 uint8_t bufferArray[MSG_MAX_LEN];
-int writer = 0;
-int reader = 0; 
-int start_byte_index= 0;
 
 uint8_t start_byte=0;
 uint8_t data_array[MSG_BYTES_MSG];
@@ -35,184 +32,81 @@ uint8_t crc1 = 0;
 uint8_t crc2 = 0;
 uint16_t calculated_crc = 0;
 
-uint8_t expected_length = 0;
-uint8_t actual_length = 0;
 
 int failed_sends = 0;
 uint8_t ACK= 0x1;
 uint8_t NACK = 0x0;
     
-    char message[MSG_BYTES_MSG];
+char message[MSG_BYTES_MSG];
 
 
-void __attribute__((__interrupt__)) _T1Interrupt(void){
-    
+void __attribute__((__interrupt__,no_auto_psv)) _T1Interrupt(void){
+    failed_sends++;
+    //    uart2_send_8(NACK); // ASKKSKSKSKSKSKSKKSKKSKS
     CLEARBIT(IFS0bits.T1IF);
     timer_expired = 1;
-}
-
-void __attribute__((__interrupt__, no_auto_psv)) _U2RXInterrupt() {
-    bufferArray[writer]= usart_getc();
-    writer = (writer + 1) % MSG_MAX_LEN;
-    IFS1bits.U2RXIF = 0;
-}
-
-uint8_t usart_getc(){
-    // Fresh data
-    uint8_t data = 0;
-    
-    //Set the timer
-    set_timer1(32767);
-        
-    while(1){
-        if (uart2_recv(&data) == 0){
-            if (data == 0x0) {
-                start_byte_index = reader;
-            }
-            reader = (reader + 1) % MSG_MAX_LEN;
-            break;
-        }
-        
-        if (timer_expired==1){
-            CLEARBIT(IEC0bits.T1IE);
-            return -1; // NEEDS TO BE FIXED ITS NOT -1 ITS SMTH ELSE
-        }
-    }
-    
-    CLEARBIT(IEC0bits.T1IE);
-    
-    return data;
-    
-//        while(1) {
-//
-//            if((uart2_recv(&start_byte)==0) && ( start_byte== 0x0)){
-//                break;
-//            }
-//            if(timer_expired == 1){
-//                CLEARBIT(IEC0bits.T1IE);
-//                printf("MESSED UP with first byte time");
-//                // Indicate time error            break;
-//                uart2_send_8(NACK);
-//            }
-//        }
-//
-//        // Reading the received CRC
-//        if((uart2_recv(&crc_bytes[0]) != 0) || (uart2_recv(&crc_bytes[1]) != 0)){
-//            CLEARBIT(IEC0bits.T1IE);
-//            printf("MESSED UP with crc");
-//            uart2_send_8(NACK);
-//    //        return NACK;
-//        }
-//        crc = ((uint16_t)crc_bytes[0]) | ((uint16_t)(crc_bytes[1]) << 8);
-//
-//        // Reading message body length
-//        if (uart2_recv(&expected_length) != 0){
-//            CLEARBIT(IEC0bits.T1IE);
-//            printf("MESSED UP with length");
-//            uart2_send_8(NACK);
-//    //        return NACK;
-//        }
-//        
-//
-//        // Read the message itself
-//        while(actual_length < expected_length){
-//            if(uart2_recv(&data) == 0){
-//                data_array[data_counter] = data;
-//                data_counter++;
-//                calculated_crc = crc_update(calculated_crc, data);
-//            }
-//
-//            if(timer_expired == 1){
-//                printf("MESSED UP with body message time");
-//                uart2_send_8(NACK);
-//    //            return NACK;
-//            }
-//        }
     
 }
 
 int main(void)
 {	
+    __C30_UART=1;	
+	lcd_initialize();
+    
+	lcd_clear();
+	lcd_locate(0,0);
+	lcd_printf("Hello World4!");	
+	/* Do nothing */
+	while(1){
+		
+	}
 	/* Q: What is my purpose? */
 	/* A: You pass butter. */
-	/* Q: Oh. My. God. */\
-    bufferArray = (uint8_t*)malloc(MSG_MAX_LEN * sizeof(uint8_t));
-    data_array = (uint8_t*)malloc(MSG_BYTES_MSG * sizeof(uint8_t));
+	/* Q: Oh. My. God. */
     
-    uart2_init(BAUD_9600);
+//    uint16_t message_length = MSG_MAX_LEN;
     
-    while(timer_expired != 1);
+//    uart2_init(BAUD_9600);
+//    
+//    
+//    while(1){
+//        timer_expired = 0;
+//        int i = 0;
     
-    // Disable receiver ?????? -> ASK QUESTION PLS
+//        data_array[i] = usart_getc();
+    //    while(data_array[i]!= 0x00) {
+    //        data_array[i] = usart_getc();
+    //    }
     
-    
-    start_byte = bufferArray[start_byte_index];
-    start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
-    
-    crc1 = (bufferArray[start_byte_index]);
-    start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
-    crc2 = ((bufferArray[start_byte_index]));
-    
-    crc = ((uint16_t)crc1 | ((uint16_t)crc2 << 8));
-    
-    start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
-    expected_length = bufferArray[start_byte_index];
-    
-    
-    while(start_byte_index != reader) {
-        start_byte_index = (start_byte_index + 1) % MSG_MAX_LEN;
-        data_array[actual_length] = bufferArray[start_byte_index];
-        actual_length++;
-        calculated_crc = crc_update(calculated_crc, bufferArray[start_byte_index]);
-        
-    }
-    
-    if((crc == calculated_crc) && (expected_length == actual_length)){
-            // Data chilling
-            printf("YOU CHILLING");
-            // print the failed on the lcd
-            lcd_locate(0,0);
-            lcd_clear_row(0);
-            lcd_printf("Recv fail: %d", failed_sends);
-            gotoLine(1);
-            lcd_clear_row(1);
-            lcd_printf("recv_buf:");
-            lcd_printf("%d %x %x %d", start_byte, crc1, crc2, expected_length);
-            gotoLine(2);
-            lcd_clear_row(2);
-            int i = 0;
-            while(i < actual_length){
-                lcd_printf("%d ", data_array[i]);
-                char c = data_array[i];
-                message[i] = c;
-                i++;
-            }
-            gotoLine(3);
-            lcd_printf("crc: %x crc: %x",calculated_crc, crc);
-            gotoLine(4);
-            lcd_printf("str: %s", message);
-            uart2_send_8(ACK);
-        }
-    
-    else{
-            printf("YOU NOOOTTTTTTT CHILLING");
-            failed_sends++;
-            uart2_send_8(NACK);
-    }
-    
-    
-    // Here is where you calculate the data array and also
-    
-    // Check data integrity (see below)
-    
-    
-//    while(1) {
-//    //data first seen, fire interrupt 1 second after
-//        // Stop timer we done
-//        CLEARBIT(IEC0bits.T1IE);
-//
-//        if((crc == calculated_crc) && (expected_length == actual_length)){
-//            // Data chilling
+//        set_timer1(32767);
+//        while(timer_expired != 1){
+//            i++;
+//            data_array[i] = usart_getc();
+//            if(i == 3){
+//                message_length = data_array[3] + 4;
+//            }
+//            if(i >= message_length){
+//                //TURN OFF TIMER HERE
+//                CLEARBIT(T1CONbits.TON);           
+//                break;
+//            } 
+//      
+//    if (timer_expired == 1 ){
+//        CLEARBIT(T1CONbits.TON);
+//        // Do more to go back top
+//    }
+//        };
+//    
+//    crc1 = data_array[1];
+//    crc2 = data_array[2];
+//    int j;
+//    crc = ((uint16_t)crc1 | ((uint16_t)crc2 << 8));
+//    for(j = 4; j < message_length; j++){
+//        calculated_crc = crc_update(calculated_crc,data_array[j]);
+//    }
+// 
+//    if(crc == calculated_crc){
+//        // Data chilling
 //            printf("YOU CHILLING");
 //            // print the failed on the lcd
 //            lcd_locate(0,0);
@@ -221,28 +115,31 @@ int main(void)
 //            gotoLine(1);
 //            lcd_clear_row(1);
 //            lcd_printf("recv_buf:");
-//            lcd_printf("%d %x %x %d", start_byte, crc_bytes[0], crc_bytes[1], expected_length);
+//            lcd_printf("%d %x %x %d", data_array[0], data_array[1], data_array[2], data_array[3]);
 //            gotoLine(2);
 //            lcd_clear_row(2);
-//            int i = 0;
-//            while(i < actual_length){
-//                lcd_printf("%d ", data_array[i]);
-//                char c = data_array[i];
-//                message[i] = c;
-//                i++;
+//            int k;
+//            for(k = 4; k < message_length; k++){
+//                lcd_printf("%d ", data_array[k]);
+//                char c = data_array[k];
+//                message[k-4] = c;
 //            }
 //            gotoLine(3);
 //            lcd_printf("crc: %x crc: %x",calculated_crc, crc);
 //            gotoLine(4);
 //            lcd_printf("str: %s", message);
+//            resetLCD();
+//            lcd_printf("hello");
 //            uart2_send_8(ACK);
-//        }
-//        else{
+//    }
+//    else{
 //            printf("YOU NOOOTTTTTTT CHILLING");
 //            failed_sends++;
 //            uart2_send_8(NACK);
-//        }
+//            printf("Failed\n");
 //    }
+//    }
+//    
     
     // Questions:
     // 1. How do we handle the NACK such that we have to resend the message with alot of NACKS or handle it differently and do we need this big while loop
@@ -254,4 +151,3 @@ int main(void)
     return 0;    
     
 }	
-
--- MPLABXProjects/uart.c
+++ MPLABXProjects/uart.c
@@ -11,6 +11,12 @@
 #include <p33FJ256MC710.h>
 
 #include "uart.h"
+#include "lab3.h"
+
+
+uint8_t dataArray[MSG_MAX_LEN];
+int read = -1;
+int write = 0;
 
 inline void uart2_init(uint16_t baud)
 {
@@ -19,8 +25,8 @@ inline void uart2_init(uint16_t baud)
     IEC1bits.U2TXIE = 0;
     IFS1bits.U2RXIF =0;
     IFS1bits.U2TXIF = 0;
-    TRISFbits.TRISF2 = 1; // Check if these are for UART2 as well
-    TRISFbits.TRISF3 = 0;
+    TRISFbits.TRISF4 = 1; // Check if these are for UART2 as well
+    TRISFbits.TRISF5 = 0;
     U2MODEbits.BRGH = 0;
     U2BRG = (uint32_t)800000 / baud -1;
     U2MODE = 0;
@@ -47,8 +53,22 @@ int8_t uart2_recv(uint8_t* data)
         U2STAbits.OERR=0;
     }
     if(U2STAbits.URXDA){
-        *data = U2RXREG & 0x00FF;
-        return 0;
+        if(write != read){
+            *data = U2RXREG & 0x00FF;
+            write = (write + 1) % MSG_MAX_LEN;
+            return 0;
+        }
     }
     return -1;
+}
+
+uint8_t usart_getc(){
+    while(uart2_recv(dataArray[write])!= 0);
+    uint8_t return_num = -1;
+    if(((read + 1) % MSG_MAX_LEN)!= write){
+        read = (read + 1) % MSG_MAX_LEN;
+        return_num = dataArray[read];
+    }
+    return return_num;
+
 }
