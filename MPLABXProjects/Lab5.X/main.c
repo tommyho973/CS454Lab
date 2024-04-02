f0f5d11e3bddea68fa7f41eb3abe12297df0b2ea
jianyingx12 <116234600+jianyingx12@users.noreply.github.com>
2024-04-02 19:45:04 -0400

    Add files via upload

--- MPLABXProjects/Lab5.X/main.c
+++ MPLABXProjects/Lab5.X/main.c
@@ -28,19 +28,28 @@ _FGS(GCP_OFF);
 
 
 uint8_t chan = 7;
-uint16_t duty_cycle = 0;
-uint16_t xmax = 0;
-uint16_t xmin = 0;
-uint16_t ymax = 0;
-uint16_t ymin = 0;
+int16_t duty_cycle = 0;
+int16_t xmax = 0;
+int16_t xmin = 0;
+int16_t ymax = 0;
+int16_t ymin = 0;
+uint8_t v1,v2;
+int16_t curr = 0;
 
-uint16_t curr = 0;
+double beta;
+double difference;
+double curr_difference;
+double alpha;
+double prop;
+double final;
+
+
 
 /* FIXXXX*/
 void __attribute__((__interrupt__)) _T2Interrupt( void )
 {
     /* Interrupt Service Routine code goes here */
-    motor_set_duty(chan, duty_cycle); // Write Duty Cycle value for next PWM cycle
+//    motor_set_duty(chan, duty_cycle); // Write Duty Cycle value for next PWM cycle
     IFS0bits.T2IF = 0; // Clear Timer 2 interrupt flag
 }
 
@@ -52,13 +61,11 @@ int main(void)
 	lcd_locate(0,0);
     
     lcd_printf("--- Lab 05 ---");
-    lcd_locate(1,0);
+    gotoLine(2);
     
     initialize_buttons();
-    
-    
+   
     touch_init(); // starts ADC for joystick
-    set_timer2(4000);
 //    motor_init(7);
     
     // Code for the trigger buttons debouncing
@@ -71,80 +78,118 @@ int main(void)
     touch_select_dim(0); //Select x axis
     while(1) { //Loop for X MAX
         // Debouncing 
-        uint8_t v1 = B1PORT;
+        v1 = B1PORT;
         __delay_ms(10);
-        uint8_t v2 = B1PORT;
+        v2 = B1PORT;
         __delay_ms(10); //Change back to 50 if need be
         
 // SO IT READS THE VALUES CORRECTLY BUT THE PROBLEM IS SOMETHING RELATED TO HOW WE ARE SAMPLING
         curr = touch_read();
-        lcd_clear_row(2);
-        lcd_locate(1,0);
-        lcd_printf("X-max?: %d", curr);
+        gotoLine(2);
+        lcd_printf("X-max?: %d       ", curr);
 
-        if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
+        if(BTN1_PRESSED()&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
+            
             lcd_clear_row(2);
-            lcd_locate(1,0);
-            lcd_printf("X-max: %d", curr);
+            gotoLine(2);
+            xmax = curr;
+            lcd_printf("X-max: %d      ", xmax);
             break;
-        }   
+            }
     }
-
-    if(BTN1_RELEASED()){
+    
+    
+    
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
+        
+        if(BTN1_RELEASED()){
             prev = 1;
+            gotoLine(4);
+            break;
         }
-
+    }
+    
     while(1){ // X -MIN LOOP
         // Debouncing 
-        uint8_t v1 = B1PORT;
+        v1 = B1PORT;
         __delay_ms(10);
-        uint8_t v2 = B1PORT;
+        v2 = B1PORT;
         __delay_ms(10); //Change back to 50 if need be
         
-        lcd_clear_row(3);
-        lcd_locate(2,0);
+        gotoLine(3);
         curr = touch_read();
-        lcd_printf("X-min?: %d", curr);
+        lcd_printf("X-min?: %d       ", curr);
 
         if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
             lcd_clear_row(3);
-            lcd_locate(2,0);
-            lcd_printf("X-min: %d", curr);
+            gotoLine(3);
+            xmin = curr;
+            lcd_printf("X-min: %d      ", xmin);
             break;
         }   
     }
 
-    if(BTN1_RELEASED()){
+//    while(!BTN1_RELEASED()){
+//            prev = 1;
+//        }
+    
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
+        
+        if(BTN1_RELEASED()){
             prev = 1;
+            gotoLine(5);
+            break;
         }
+    }
 
     touch_select_dim(1); //Select y axis
     while(1) { // Y - MAX LOOP
         // Debouncing 
-        uint8_t v1 = B1PORT;
+        v1 = B1PORT;
         __delay_ms(10);
-        uint8_t v2 = B1PORT;
+        v2 = B1PORT;
         __delay_ms(10); //Change back to 50 if need be
         
-        lcd_clear_row(4);
-        lcd_locate(3,0);
+        gotoLine(4);
         curr = touch_read();
-        lcd_printf("Y-max?: %d", curr);
+        lcd_printf("Y-max?: %d      ", curr);
 
-        if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
+        if(BTN1_PRESSED()&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
             lcd_clear_row(4);
-            lcd_locate(3,0);
-            lcd_printf("Y-max: %d", curr);
+            gotoLine(4);
+            ymax = curr;
+            lcd_printf("Y-max: %d      ", ymax);
             break;
         }   
     }
     
-    if(BTN1_RELEASED()){
+//    while(!BTN1_RELEASED()){
+//            prev = 1;
+//        }
+    
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
+        
+        if(BTN1_RELEASED()){
             prev = 1;
+            gotoLine(5);
+            break;
         }
+    }
 
     while(1) { // Y-min
         // Debouncing 
@@ -153,27 +198,37 @@ int main(void)
         uint8_t v2 = B1PORT;
         __delay_ms(10); //Change back to 50 if need be
         
-        lcd_clear_row(5);
-        lcd_locate(4,0);
+        gotoLine(5);
         curr = touch_read();
-        lcd_printf("Y-min?: %d", curr);
+        lcd_printf("Y-min?: %d       ", curr);
 
         if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
             lcd_clear_row(5);
-            lcd_locate(4,0);
-            lcd_printf("Y-min: %d", curr);
+            gotoLine(5);
+            ymin = curr;
+            lcd_printf("Y-min: %d      ", ymin);
             break;
         }   
     }
 
-    if(BTN1_RELEASED()){
-            prev = 1;
-        }
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
 
+        if(BTN1_RELEASED()){
+            prev = 1;
+            gotoLine(5);
+            break;
+        }
+    }
+    
     // Move the X motor based on what the X position is
     motor_init(8);
     touch_select_dim(0);
+    difference = (xmax - xmin);
     while(1){
         uint8_t v1 = B1PORT;
         __delay_ms(10);
@@ -181,28 +236,52 @@ int main(void)
         __delay_ms(10); //Change back to 50 if need be
 
         curr = touch_read();
-        duty_cycle; // Math to calculate the duty cycle
-        motor_set_duty(8, duty_cycle);
-        lcd_clear_row(6);
-        lcd_locate(5,0);
-        lcd_printf("PW X?: %d", duty_cycle)
         
-        if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
+        curr_difference = curr - xmin;
+        beta = curr_difference/difference;
+        gotoLine(7);
+        if (beta < 0){
+            beta = 0;
+        }
+        alpha = (.9 + (1.200*beta))*1000;
+        uint16_t alpha_int = alpha;
+        
+        prop = alpha/20000;
+        final = 4000 -(4000*(prop));
+        
+        duty_cycle = final; // Math to calculate the duty cycle
+        
+        motor_set_duty(8, duty_cycle);
+        gotoLine(6);
+        lcd_printf("PW X?: %d          ", alpha_int);
+        
+        if(BTN1_PRESSED()&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
             lcd_clear_row(6);
-            lcd_locate(5,0);
-            lcd_printf("PW X: %d", duty_cycle);
+            gotoLine(6);
+            lcd_printf("PW X: %d        ", alpha_int);
             break;
+
         }   
     }
 
-    if(BTN1_RELEASED()){
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
+        
+        if(BTN1_RELEASED()){
             prev = 1;
+            gotoLine(6);
+            break;
         }
+    }
 
     // Move the Y motor based on what the Y position is
     motor_init(7);
     touch_select_dim(1);
+    difference = (ymax - ymin);
     while(1){
         uint8_t v1 = B1PORT;
         __delay_ms(10);
@@ -210,24 +289,64 @@ int main(void)
         __delay_ms(10); //Change back to 50 if need be
 
         curr = touch_read();
-        duty_cycle; // Math to calculate the duty cycle
+        
+        
+        curr_difference = curr - ymin;
+        beta = curr_difference/difference;
+        gotoLine(7);
+        if (beta < 0){
+            beta = 0;
+        }
+        alpha = (.9 + (1.200*beta))*1000;
+        uint16_t alpha_int = alpha;
+        prop = alpha/20000;
+        final = 4000 -(4000*(prop));
+        
+        duty_cycle = final; // Math to calculate the duty cycle
         motor_set_duty(7, duty_cycle);
-        lcd_clear_row(7);
-        lcd_locate(6,0);
-        lcd_printf("PW Y?: %d", duty_cycle)
+
+        gotoLine(7);
+        lcd_printf("PW Y?: %d       ", alpha_int)
         
         if(BTN1_PRESSED()&&(v2 <.5)&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
             prev = v2;
             lcd_clear_row(7);
-            lcd_locate(6,0);
-            lcd_printf("PW Y: %d", duty_cycle);
+            gotoLine(7);
+            lcd_printf("PW Y: %d             ", alpha_int);
             break;
         }   
     }
 
-    if(BTN1_RELEASED()){
+    while (1){
+        v1 = B1PORT;
+        __delay_ms(50);
+        v2 = B1PORT;
+        __delay_ms(50); 
+        
+        if(BTN1_RELEASED()){
             prev = 1;
+            break;
         }
-
+    }
+   
+    while(1){
+//        uint8_t v1 = B1PORT;
+//        __delay_ms(10);
+//        uint8_t v2 = B1PORT;
+//        __delay_ms(10);
+//        
+//        
+//        if(BTN1_PRESSED()&& (v2 != prev)){ //If you know that you have pressed the button, then you can take a snapshot of the x axis max 
+//            prev = v2;
+//            break;
+//        }
+//        
+////        if(BTN1_RELEASED()){
+////            prev = 1;
+////            break;
+////        }
+        
+    }
+    
     return 0;
 }
--- MPLABXProjects/Lab5.X/motor.c
+++ MPLABXProjects/Lab5.X/motor.c
@@ -18,31 +18,55 @@ void motor_init(uint8_t chan) {
 
     if (chan == 7){
         //setup OC7
+        
+        // Initialize Output Compare Module for the Y SERVO MOTOR
+        CLEARBIT(T2CONbits.TON); // Disable Timer
+        CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
+        CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
+        TMR2 = 0x00; // Clear timer register
+        T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
+        CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
+        CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
+        PR2 = 4000; // Set timer period 20ms:
+        
         CLEARBIT(TRISDbits.TRISD6); /* Set OC7 as output */
-        OC7R = 180; /* Set the initial duty cycle to 2.1ms calculated as timer tick duration(5micro seconds) * OC8R = 0.9ms*/
-        OC7RS = 180; /* Load OCRS: next pwm duty cycle */
+        OC7R = 3580; /* Set the initial duty cycle to 2.1ms calculated as timer tick duration(5micro seconds) * OC8R = 0.9ms*/
+        OC7RS = 3580; /* Load OCRS: next pwm duty cycle */
         OC7CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 */
+        
+        SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
         
     }
     else {  
         //setup OC8
+        // Initialize Output Compare Module for the Y SERVO MOTOR
+        CLEARBIT(T2CONbits.TON); // Disable Timer
+        CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
+        CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
+        TMR2 = 0x00; // Clear timer register
+        T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
+        CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
+        CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
+        PR2 = 4000; // Set timer period 20ms:
+        
         CLEARBIT(TRISDbits.TRISD7); /* Set OC8 as output */
-        OC8R = 180; /* Set the initial duty cycle to 2.1ms calculated as timer tick duration(5micro seconds) * OC8R = 0.9 ms*/
-        OC8RS = 180; /* Load OCRS: next pwm duty cycle */
+        OC8R = 4000; /* Set the initial duty cycle to 2.1ms calculated as timer tick duration(5micro seconds) * OC8R = 0.9 ms*/
+        OC8RS = 4000; /* Load OCRS: next pwm duty cycle */
         OC8CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 */
         
+        SETBIT(T2CONbits.TON); /* Turn Timer 2 on */ 
     }
     
-        SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
+        
    
 }
 
     void motor_set_duty(uint8_t chan, uint16_t duty_us) {
     if (chan == 7){
-        OC7RS = (duty_us / 5);
+        OC7RS = duty_us;
 
     }
     else {
-        OC8RS = (duty_us / 5);
+        OC8RS = duty_us;
     }
 }
